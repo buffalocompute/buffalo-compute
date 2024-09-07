233e0acd5130d9d30d7059680ec392f247e2bcd0
Joe Alongi <36609816+heyitsjoealongi@users.noreply.github.com>
2024-09-06 23:34:51 -0400

    Swiper

--- buffalocompute.client/src/app/app.module.ts
+++ buffalocompute.client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { NavbarComponent } from './menu/navbar/navbar.component';
+import { SwiperComponent } from './swiper/swiper.component';
 import { FooterComponent } from './menu/footer/footer.component';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { FooterComponent } from './menu/footer/footer.component';
     AppRoutingModule,
     SharedModule,
     NavbarComponent,
+    SwiperComponent,
     FooterComponent,
   ],
   providers: [],
